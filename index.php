d9579f6322b136b5bdeca952f14c5e955671e2d9
Pablo Romero <pabloandresromero@yahoo.com.mx>
2019-10-22 15:51:10 -0500

    imagenes

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"> -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/1a6576ed2d.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.2/css/all.css">
     <title>Chabtic</title>
 </head>
@@ -48,12 +49,12 @@
 
                 <!-- Left and right controls -->
                 <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left"></span>
-                    <span class="sr-only">Previous</span>
+                    <!-- <span ><i class="fas fa-angle-right"></i></span> -->
+                    <!-- <span class="sr-only">Previous</span> -->
                 </a>
                 <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right"></span>
-                    <span class="sr-only">Next</span>
+                    <!-- <span><i class="fas fa-angle-right"></i></span> -->
+                    <!-- <span class="sr-only">Next</span> -->
                 </a>
             </div>
         </div>
